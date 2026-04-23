98c4efcba445e06f69ee16adb0f8facbafbb6c8d
Harshitha628 <11249A315@kanchiuniv.ac.in>
2026-04-23 11:12:08 +0530

    Create Data structures.r

---
+++ Data structures.r
@@ -0,0 +1,108 @@
+marks <- c(55, 65, 75, 88, 95)
+total <- sum(marks)
+average <- mean(marks)
+highest <- max(marks)
+total
+average
+highest
+
+temperature <- c(10, 20, 25, 44, 25, 55, 33)
+min_temp <- min(temperature)
+max_temp <- max(temperature)
+min_temp
+max_temp
+
+employee <- list(
+ID = 315,
+Name = "harshitha",
+Salary = 45000,
+Department = "IT"
+)
+employee
+employee$Name
+employee$Salary
+
+
+patient <- list(
+PatientID = 202,
+Name = "Raju",
+Age = 55,
+TestResults = c(120, 140, 130)
+)
+patient
+patient$TestResults
+
+
+students <- data.frame(
+RollNo = c(1, 2, 3),
+Name = c("harshitha", "Karthik", "pranitha"),
+Dept = c("CSE", "ECE", "IT"),
+Marks = c(85, 78, 92)
+)
+students
+
+
+sales <- data.frame(
+Month = c("Jan", "Feb", "Mar"),
+Sales = c(50000, 62000, 58000)
+)
+total_sales <- sum(sales$Sales)
+total_sales
+
+
+prices <- c(1200, 850, 560)
+customer <- list(
+CustomerID = 301,
+Name = "Anjali",
+City = "Chennai"
+)
+orders <- data.frame(
+Item = c("Phone", "Headset", "Charger"),
+Price = prices
+)
+prices
+customer
+orders
+
+
+subject_marks <- c(78, 85, 88)
+student_profile <- list(
+Name = "Arun",
+RollNo = 105,
+Marks = subject_marks
+)
+class_results <- data.frame(
+RollNo = c(101, 102, 103),
+Total = c(240, 255, 270)
+)
+student_profile
+class_results
+
+
+balances <- c(5000, 5200, 5100)
+customer <- list(
+AccountNo = 954232,
+Name = "harshitha"
+)
+transactions <- data.frame(
+Date = c("01-01", "02-01", "03-01"),
+Amount = c(-600, 7000, -600)
+)
+balances
+customer
+transactions
+
+
+
+profile <- list(
+Name = "priya",
+Age = 19,
+Scores = scores
+  )
+records <- data.frame(
+Subject = c("Math", "CS", "Physics"),
+Marks = scores
+)
+
+
+
